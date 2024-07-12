7f2a9566778739a3c87a76fb6cb50290b060c63b
Jack Beagley <jack.beagley@outlook.com>
2024-07-12 13:52:41 +1200

    Adding comments, stability to plots

--- test.py
+++ test.py
@@ -28,6 +28,8 @@ derived = np.empty((n_z, n_t, bd.indices.n_derived), order='F')
 
 z = np.linspace(-120, -430, n_z)
 # z = np.linspace(0.0, -10.0, n_z)
+# sa[:, 0] = np.linspace(35.32, 35.24, n_z)
+# tis[:, 0] = np.linspace(13.0, 12.2, n_z)
 sa[:, 0] = np.linspace(35.32, 35.24, n_z)
 tis[:, 0] = np.linspace(13.0, 12.2, n_z)
 
@@ -51,13 +53,17 @@ t = np.linspace(t_start, t_end, n_t)
 dt = (t_end - t_start) / (n_t - 1)
 dz = (z[-1] - z[0]) / (n_z - 1)
 
-sa_bc = sa_offshore_f(t/year_seconds*365)
-tis_bc = tis_offshore_f(t/year_seconds*365)
+# approximated from Moana
+# sa_bc = sa_offshore_f(t/year_seconds*365)
+# tis_bc = tis_offshore_f(t/year_seconds*365)
+sa_bc = np.full_like(t, 35.23)
+tis_bc = np.full_like(t, 12.9)
+
 
 kappa_buoyancy_coefs = [2e-7, 1.6]
-kappa_const = 1e-4
+kappa_const = 1e-3
 
-bd.basin.compute(z, a, sa, tis, derived, t, sa_bc, tis_bc, bd.computation_modes.closed_top, bd.diffusivity_modes.constant, [kappa_const, np.nan])
+bd.basin.compute(z, a, sa, tis, derived, t, sa_bc, tis_bc, bd.computation_modes.normal, bd.diffusivity_modes.constant, [kappa_const, np.nan])
 
 kappa = derived[:, :, bd.indices.kappa-1]
 rho = derived[:, :, bd.indices.rho-1]
@@ -80,12 +86,25 @@ kappa_py[np.isinf(kappa_py)] = np.nan
 
 kappa_range = np.nanquantile(kappa, [0.01, 0.99])
 
+# calculate the area-weighted mean salinity and temperature
+sa_mean = np.sum(sa[:, 0] * a) / np.sum(a)
+tis_mean = np.sum(tis[:, 0] * a) / np.sum(a)
+
+max_sa_deviation = np.max(np.abs(sa[:, 0] - sa_mean))
+max_tis_deviation = np.max(np.abs(tis[:, 0] - tis_mean))
+
+# calculate the sa and tis deviations as a percentage of maximum
+sa_deviation_percent = (sa - sa_mean) / max_sa_deviation * 100
+tis_deviation_percent = (tis - tis_mean) / max_tis_deviation * 100
+
 t_months = t/year_seconds*12
 
 tt_months, zz = np.meshgrid(t_months, z)
 
 # Make a heatmap of salinity evolution
 fig = plt.figure()
+
+## SALINITY
 ax_sal = fig.add_subplot(3, 2, 1)
 ax_sal.set_xlabel('Time / months')
 ax_sal.set_ylabel('Depth / m')
@@ -101,6 +120,7 @@ cbar = ax_sal.figure.colorbar(ax_sal.images[0], ax=ax_sal, label='Absolute Salin
 cont = ax_sal.contour(tt_months, zz, sa, levels=np.linspace(30, 40, 101), colors='k', linewidths=0.5)
 ax_sal.clabel(cont, inline=True, fontsize=10, fmt='%1.1f')
 
+## TEMPERATURE
 # Make a heatmap of temperature evolution
 ax_temp = fig.add_subplot(3, 2, 3)
 ax_temp.set_xlabel('Time / months')
@@ -115,7 +135,7 @@ cbar = ax_temp.figure.colorbar(ax_temp.images[0], ax=ax_temp, label='Temperature
 cont = ax_temp.contour(tt_months, zz, tis, levels=np.linspace(0, 20, 51), colors='k', linewidths=0.5)
 ax_temp.clabel(cont, inline=True, fontsize=10, fmt='%1.1f')
 
-# density
+## DENSITY
 ax_sigma0 = fig.add_subplot(3, 2, 5)
 ax_sigma0.set_xlabel('Time / months')
 ax_sigma0.set_ylabel('Depth / m')  
@@ -134,8 +154,17 @@ ax_sigma0.imshow(instability_mask_rgb, extent=heatmap_extent, aspect='auto', alp
 cont = ax_sigma0.contour(tt_months, zz, sigma0, levels=np.linspace(20, 40, 201), colors='k', linewidths=0.5)
 ax_sigma0.clabel(cont, inline=True, fontsize=10, fmt='%1.1f')
 
-# diffusivity - kappa
-ax_kappa = fig.add_subplot(3, 2, 2)
+## BRUNT-VAISALA FREQUENCY
+ax_n = fig.add_subplot(3, 2, 2)
+ax_n.set_xlabel('Time / months')
+ax_n.set_ylabel('Depth / m')
+
+ax_n.imshow(N, cmap=cmo.cm.speed, aspect='auto', extent=heatmap_extent)
+
+cbar = ax_n.figure.colorbar(ax_n.images[0], ax=ax_n, label='Brunt-Vaisala frequency / s$^{-1}$')
+
+# DIFFUSIVITY
+ax_kappa = fig.add_subplot(3, 2, 4)
 ax_kappa.set_xlabel('Time / months')
 ax_kappa.set_ylabel('Depth / m')
 
@@ -144,23 +173,15 @@ ax_kappa.imshow(kappa, cmap=cmo.cm.amp, aspect='auto', extent=heatmap_extent, vm
 # Add a colorbar
 cbar = ax_kappa.figure.colorbar(ax_kappa.images[0], ax=ax_kappa, label='Diffusivity / m$^2$s$^{-1}$')
 
-# Brunt-Vaisala frequency
-ax_n = fig.add_subplot(3, 2, 4)
-ax_n.set_xlabel('Time / months')
-ax_n.set_ylabel('Depth / m')
+## QUANTITY DEVIATIONS
+ax_deviation = fig.add_subplot(3, 2, 6)
+ax_deviation.set_xlabel('Time / months')
+ax_deviation.set_ylabel('Deviation / %')
 
-ax_n.imshow(N, cmap=cmo.cm.speed, aspect='auto', extent=heatmap_extent)
+ax_deviation.plot(t_months, np.max(np.abs(sa_deviation_percent), axis=0), color='k', label='Salinity')
+ax_deviation.plot(t_months, np.max(np.abs(tis_deviation_percent), axis=0), color='r', label='Temperature')
 
-cbar = ax_n.figure.colorbar(ax_n.images[0], ax=ax_n, label='Brunt-Vaisala frequency / s$^{-1}$')
-
-# python derived diffusivity
-ax_kappa_py = fig.add_subplot(3, 2, 6)
-ax_kappa_py.set_xlabel('Time / months')
-ax_kappa_py.set_ylabel('Depth / m')
-
-ax_kappa_py.imshow(kappa_py, cmap=cmo.cm.amp, aspect='auto', extent=heatmap_extent, vmin=kappa_range[0], vmax=kappa_range[1])
-
-cbar = ax_kappa_py.figure.colorbar(ax_kappa_py.images[0], ax=ax_kappa_py, label='Diffusivity / m$^2$s$^{-1}$')
+ax_deviation.legend()
 
 plt.tight_layout()
 
@@ -170,7 +191,7 @@ ax_sigma0 = fig.add_subplot(1, 4, 1)
 ax_sigma0.plot(sigma0[:, 0], z, color='k', label='Initial')
 ax_sigma0.plot(sigma0[:, -2], z, color='r', label='Final')
 
-ax_sigma0.set_xlabel('Sigma0 Density / kgm$^{-3}$')
+ax_sigma0.set_xlabel('Sigma-$\\theta$ Density \n/ $kgm^{-3}$')
 ax_sigma0.set_ylabel('Depth / m')
 
 ax_sigma0.legend()
@@ -179,7 +200,7 @@ ax_sal = fig.add_subplot(1, 4, 2, sharey=ax_sigma0)
 ax_sal.plot(sa[:, 0], z, color='k', label='Initial')
 ax_sal.plot(sa[:, -2], z, color='r', label='Final')
 
-ax_sal.set_xlabel('Absolute Salinity / gkg$^{-1}$')
+ax_sal.set_xlabel('Absolute Salinity \n/ gkg$^{-1}$')
 # ax_sal.set_ylabel('Depth / m')
 
 ax_sal.legend()
@@ -188,7 +209,7 @@ ax_temp = fig.add_subplot(1, 4, 3, sharey=ax_sigma0)
 ax_temp.plot(tis[:, 0], z, color='k', label='Initial')
 ax_temp.plot(tis[:, -2], z, color='r', label='Final')
 
-ax_temp.set_xlabel('Temperature / $^{\circ}$C')
+ax_temp.set_xlabel('Temperature \n/ $^{\circ}$C')
 # ax_temp.set_ylabel('Depth / m')
 
 ax_temp.legend()
@@ -197,7 +218,7 @@ ax_temp.legend()
 ax_hypso = fig.add_subplot(1, 4, 4, sharey=ax_sigma0)
 ax_hypso.plot(a/1000000, z, color='k')
 
-ax_hypso.set_xlabel('Area / km$^{2}$')
+ax_hypso.set_xlabel('Area \n/ km$^{2}$')
 # ax_hypso.set_ylabel('Depth / m')
 
 for ax in [ax_sigma0, ax_sal, ax_temp, ax_hypso]:
@@ -205,5 +226,7 @@ for ax in [ax_sigma0, ax_sal, ax_temp, ax_hypso]:
 
 for ax in [ax_sal, ax_temp, ax_hypso]:
     plt.setp(ax.get_yticklabels(), visible=False)
+
+plt.tight_layout()
 
 plt.show()
