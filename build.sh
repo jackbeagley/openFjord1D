985057994f20018eb39a206bcffa4b5302202f13
Jack Beagley <jack.beagley@outlook.com>
2024-10-31 11:49:02 +1300

    Added lapack as gitmodule, will use if no lapack found

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -S./src -B./build -G Ninja -DGSW_PATH=../extern/gsw
+cmake -S./src -B./build -G Ninja -DEXTERN_DIR=../extern
 cmake --build ./build
--- setup.py
+++ setup.py
@@ -6,10 +6,8 @@ from pathlib import Path
 
 from skbuild import setup
 
-from setuptools import Extension
-from setuptools.command.build_ext import build_ext
 from setuptools.command.develop import develop
-from setuptools.command.install import install
+
 
 # Convert distutils Windows platform specifiers to CMake -A arguments
 PLAT_TO_CMAKE = {
@@ -31,133 +29,8 @@ class CustomDevelopCommand(develop):
 		initialise_submodules(self.project_dir)
 		develop.run(self)
 
-class CustomInstallCommand(install):
-	def __init__(self, project_dir):
-		install.__init__(self)
-		self.project_dir = project_dir
-
-	def run(self):
-		initialise_submodules(self.project_dir)
-		install.run(self)
-
-# A CMakeExtension needs a sourcedir instead of a file list.
-# The name must be the _single_ output extension from the CMake build.
-# If you need multiple extensions, see scikit-build.
-class CMakeExtension(Extension):
-    def __init__(self, name: str, sourcedir: str = "src") -> None:
-        super().__init__(name, sources=[])
-        self.sourcedir = os.fspath(Path(sourcedir).resolve())
-
-
-class CMakeBuild(build_ext):
-    def build_extension(self, ext: CMakeExtension) -> None:
-        package_dir = Path(__file__).resolve().parent
-		
-		# Make sure git submodules are initialised
-        initialise_submodules(str(package_dir))
-
-        # Must be in this form due to bug in .resolve() only fixed in Python 3.10+
-        ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)
-        extdir = ext_fullpath.parent.resolve()
-
-        # Using this requires trailing slash for auto-detection & inclusion of
-        # auxiliary "native" libs
-
-        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
-        cfg = "Debug" if debug else "Release"
-
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
-
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
-        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
-        # from Python.
-        cmake_args = [
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
-            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
-        ]
-        build_args = []
-        # Adding CMake arguments set as environment variable
-        # (needed e.g. to build for ARM OSx on conda-forge)
-        if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
-
-        # In this example, we pass in the version to C++. You might not need to.
-        cmake_args += [f"-DEXAMPLE_VERSION_INFO={self.distribution.get_version()}"]
-
-        if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreads automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
-            if not cmake_generator or cmake_generator == "Ninja":
-                try:
-                    import ninja
-
-                    ninja_executable_path = Path(ninja.BIN_DIR) / "ninja"
-                    cmake_args += [
-                        "-GNinja",
-                        f"-DCMAKE_MAKE_PROGRAM:FILEPATH={ninja_executable_path}",
-                    ]
-                except ImportError:
-                    pass
-
-        else:
-            # Single config generators are handled "normally"
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-
-            # CMake allows an arch-in-generator style for backward compatibility
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
-
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
-
-            # Multi-config generators have a different way to specify configs
-            if not single_config:
-                cmake_args += [
-                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
-                ]
-                build_args += ["--config", cfg]
-
-        if sys.platform.startswith("darwin"):
-            # Cross-compile support for macOS - respect ARCHFLAGS if set
-            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
-            if archs:
-                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
-
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += [f"-j{self.parallel}"]
-
-		# Add the GSW path to the CMake arguments (should be in git submodule extern/gsw relative to this file
-        gsw_path = os.path.abspath(os.path.join(package_dir, 'extern', 'gsw'))
-
-        cmake_args += [f"-DGSW_PATH={gsw_path}"]
-
-        build_temp = Path(self.build_temp) / ext.name
-        if not build_temp.exists():
-            build_temp.mkdir(parents=True)
-
-        subprocess.run(
-            ["cmake", ext.sourcedir, *cmake_args], cwd=build_temp, check=True
-        )
-        subprocess.run(
-            ["cmake", "--build", ".", *build_args], cwd=build_temp, check=True
-        )
-
 package_dir = Path(__file__).resolve().parent
-gsw_path = os.path.abspath(os.path.join(package_dir, 'extern', 'gsw'))
+extern_dir = os.path.abspath(os.path.join(package_dir, 'extern'))
 
 initialise_submodules(str(package_dir))
 
@@ -172,12 +45,10 @@ setup(
 #    long_description="",
 #    ext_modules=[CMakeExtension("openFjord1D")],
     cmake_source_dir="src",
-	cmake_args=[f"-DGSW_PATH={gsw_path}"],
-    cmdclass={
-#		"build_ext": CMakeBuild,
-		"develop": CustomDevelopCommand,
-		"install": CustomInstallCommand,
-		},
+	cmake_args=[f"-DEXTERN_DIR={extern_dir}"],
+#    cmdclass={
+#		"develop": CustomDevelopCommand,
+#		},
     zip_safe=False,
     python_requires=">=3.11",
 	install_requires=[
